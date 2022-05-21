30c844cc04f83a59fd1443266e445bf50954a16a
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2022-05-21 23:37:55 +0600

    containers added

---
+++ src/containers/IceCreamBuilder/IceCreamBuilder.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import classes from "./IceCreamBuilder.module.css";
+
+export default class IceCreamBuilder extends Component {
+  render() {
+    return;
+    <div className={["container", classes.container].join("")}>
+        
+    </div>;
+  }
+}
